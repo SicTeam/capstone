0c55d4a7139a14fbc9288076e20ee9d598ad37d4
Dakota Sanchez <dakota@dakotasanchez.com>
2018-03-04 17:16:47 -0800

    Keep track of corresponding frames

--- ros/src/sd5/src/ros_node.cpp
+++ ros/src/sd5/src/ros_node.cpp
@@ -8,53 +8,93 @@
 #include "../../../../tracking/drone.h"
 
 mavros_msgs::State current_state;
-Track drone_track;
-int count = 0;
 
+// Drone detecting/tracking object
+Track drone_track;
+
+// Keep counts to only use every 10th frame
+int left_count = 0;
+int right_count = 0;
+
+// Keep track of last seen frame (to sync corresponding left and right frames)
+cv::Mat * last_image = 0;
+
+// Get current mavros state
 void state_cb(const mavros_msgs::State::ConstPtr& msg) {
     current_state = *msg;
 }
 
 // Gets called when a left image frame comes in
 void image_callback_left(const sensor_msgs::ImageConstPtr& msg) {
-    ROS_INFO("Received left image with size: %i x %i", msg->width, msg->height);
+    if (left_count == 10) {
+        ROS_INFO("Received left image");
 
-    cv_bridge::CvImagePtr cv_ptr;
-    try {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
+        // Extract cv::Mat from message
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        cv::Mat left_image = cv_ptr->image;
+
+        // Check if it corresponds with saved last image
+        if (last_image) {
+            std::cout << "This image corresponds with the last saved image" << std::endl;
+            last_image = 0;
+        } else {
+            last_image = &left_image;
+        }
+
+        // Detect and draw bounding boxes on frame
+        std::vector<cv::Rect> drones;
+        drone_track.detect(drones, left_image);
+
+        left_count = 0;
+    } else {
+        left_count++;
     }
-
-    cv::Mat left_image = cv_ptr->image;
-    std::vector<cv::Rect> drones;
-
-    drone_track.detect(drones, left_image);
 }
 
 // Gets called when a right image frame comes in
 void image_callback_right(const sensor_msgs::ImageConstPtr& msg) {
-    ROS_INFO("Received right image with size: %i x %i", msg->width, msg->height);
+    if(right_count == 10) {
+        ROS_INFO("Received right image");
 
-    cv_bridge::CvImagePtr cv_ptr;
-    try {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
+        // Extract cv::Mat from message
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        cv::Mat right_image = cv_ptr->image;
+
+        // Check if it corresponds with saved last image
+        if (last_image) {
+            std::cout << "This image corresponds with the last saved image" << std::endl;
+            last_image = 0;
+        } else {
+            last_image = &right_image;
+        }
+
+        // Detect and draw bounding boxes on frame
+        std::vector<cv::Rect> drones;
+        drone_track.detect(drones, right_image);
+
+        right_count = 0;
+    } else {
+        right_count++;
     }
-
-    cv::Mat right_image = cv_ptr->image;
-    std::vector<cv::Rect> drones;
-
-    drone_track.detect(drones, right_image);
 }
 
 // Gets called when a left image frame comes in
 void image_callback_rear(const sensor_msgs::ImageConstPtr& msg) {
-    ROS_INFO("Received rear image with size: %i x %i", msg->width, msg->height);
+    ROS_INFO("Received rear image");
 
+    // Extract cv::Mat from message
     cv_bridge::CvImagePtr cv_ptr;
     try {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -62,10 +102,9 @@ void image_callback_rear(const sensor_msgs::ImageConstPtr& msg) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-
     cv::Mat rear_image = cv_ptr->image;
-    std::vector<cv::Rect> drones;
 
+    std::vector<cv::Rect> drones;
     drone_track.detect(drones, rear_image);
 }
 
@@ -82,8 +121,8 @@ int main(int argc, char **argv) {
             2, image_callback_left);
     ros::Subscriber right_sub = nh.subscribe("/sd5_1/camera_sd5_right/image_raw",
             2, image_callback_right);
-    ros::Subscriber rear_sub = nh.subscribe("/sd5_1/camera_sd5_rear/image_raw",
-            2, image_callback_rear);
+    //ros::Subscriber rear_sub = nh.subscribe("/sd5_1/camera_sd5_rear/image_raw",
+    //        2, image_callback_rear);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("/sd5_1/mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
