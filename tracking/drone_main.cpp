17ffb7a5634dbf6b3cbd05512d7d799d16d9c811
Khuong <khuongnguyen232@gmail.com>
2018-03-09 17:27:50 -0800

    Added tracking test

--- tracking/drone_main.cpp
+++ tracking/drone_main.cpp
@@ -16,14 +16,17 @@ int main(int argc, char ** argv)
     
     std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
     std::cout << "OpenCV Minor Version: " << CV_MINOR_VERSION << std:: endl;
-    //for testing the detection
-//    drone_track.detect_image(argv[1]);
+
+    //uncomment for testing the detection
 
     //drone_track.detect_image(argv[1]);
 
+    //uncomment for testing the tracking
+
+    drone_track.track_tester(argv[1]);
+
     //detect/track off video passed in, else use live video feed
 
-    drone_track.kcf(argv[1],argv[2]);
     /*if(argv[1])
     {
         drone_track.kcf(argv[1]);
