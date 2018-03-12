71a13640aa7d3de694d48377a6b4606ad1b334ac
Khuong <khuongnguyen232@gmail.com>
2018-03-11 17:18:55 -0700

    Create Testing File and modify CMakeList.txt

---
+++ tracking/detect_test.cpp
@@ -0,0 +1,12 @@
+#include "drone.h"
+
+int main(int argc, char ** argv)
+{
+    Track drone_track;
+
+    if(argc > 1)    
+    	drone_track.detect_image(argv[1]);
+    else
+	std::cout << "Need an image arguement followed after detect" << std::endl;
+    return 0;
+}
