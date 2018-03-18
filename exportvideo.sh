a21d42f84452399f2c3d2b8a526fd889fe7b3370
dragonrider0601 <dragonrider0601@gmail.com>
2018-03-18 14:32:20 -0700

    no email

--- exportvideo.sh
+++ exportvideo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Author: Samuel Strba
-#Email: sstrba@pdx.edu
 #Run this script when you are running the multiple drones Gazebo simulation.
 #The script grabs the video feeds from the cameras in Gazebo and saves them to .avi files in the ~/Videos folder.
 rosrun image_view video_recorder image:=/sd5_1/camera_sd5_left/image_raw _filename:=$HOME/Videos/output_left.avi _codec:=MJPG &
